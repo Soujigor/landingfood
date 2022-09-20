8b4f6cb3e23378fe51887e82a9fb0854f0ea8e4a
Igor Souza <soujigor@gmail.com>
2022-09-20 17:48:33 -0300

    editando form

--- components/form/Form.js
+++ components/form/Form.js
@@ -8,6 +8,9 @@ import {
   NumberInputField,
   Textarea,
   Stack,
+  Radio,
+  RadioGroup,
+  Grid,
 } from "@chakra-ui/react";
 
 const Form = () => {
@@ -17,10 +20,10 @@ const Form = () => {
       justifyContent="center"
       alignContent="center"
     >
-      <Stack direction={["column", "column", "row", "row"]} spacing="50px">
+      <Grid padding="20px" flexDirection={["column", "column", "row", "row"]} templateColumns="repeat(4, 1fr)" gap="50px">
         <FormControl isRequired>
           <FormLabel>Nome Completo</FormLabel>
-          <Input />
+          <Textarea resize="none" />
         </FormControl>
         <FormControl isRequired>
           <NumberInput>
@@ -30,13 +33,73 @@ const Form = () => {
         </FormControl>
         <FormControl isRequired>
           <FormLabel>Endereço Completo</FormLabel>
-          <Input />
+          <Textarea resize="none" />
         </FormControl>
         <FormControl isRequired>
           <FormLabel>Conte-me sobre sua rotina</FormLabel>
-          <Textarea resize="none"/>
+          <Textarea resize="none" />
         </FormControl>
-      </Stack>
+        <FormControl isRequired>
+          <FormLabel>Proteínas Preferidas</FormLabel>
+          <Textarea resize="none" />
+        </FormControl>
+        <FormControl isRequired>
+          <FormLabel>Legumes que não podem faltar</FormLabel>
+          <Textarea resize="none" />
+        </FormControl>
+        <FormControl isRequired>
+          <FormLabel>O que você não come de jeito nenhum</FormLabel>
+          <Textarea resize="none" />
+        </FormControl>
+        <FormControl isRequired>
+          <FormLabel>O prato que não pode faltar</FormLabel>
+          <Textarea resize="none" />
+        </FormControl>
+
+        <FormControl isRequired>
+          <FormLabel>Como pretende armazenar?</FormLabel>
+          <RadioGroup defaultValue="">
+            <Stack spacing={5} direction={["column", "column", "row", "row"]}>
+              <Radio value="1">
+                Porções separadas já montadas para a sua refeição
+              </Radio>
+              <Radio value="2">
+                Porções livres, alimentos cozidos e guardados separadamente
+              </Radio>
+            </Stack>
+          </RadioGroup>
+        </FormControl>
+
+        <FormControl isRequired>
+          <FormLabel>Para armazenamento da comida, você prefere:</FormLabel>
+          <RadioGroup defaultValue="">
+            <Stack spacing={5} direction={["column", "column", "row", "row"]}>
+              <Radio value="1">Utilizar os próprios recipientes</Radio>
+              <Radio value="2">
+                Embalagens levadas por mim (contém custo adicional)
+              </Radio>
+            </Stack>
+          </RadioGroup>
+        </FormControl>
+
+        <FormControl isRequired>
+          <FormLabel>
+            Quantos dias de consumação dos alimentos produzidos?
+          </FormLabel>
+          <RadioGroup defaultValue="">
+            <Stack spacing={5} direction={["column", "column", "row", "row"]}>
+              <Radio value="1">7 dias</Radio>
+              <Radio value="2">10 dias</Radio>
+              <Radio value="3">15 dias</Radio>
+              <Radio value="4">20 dias</Radio>
+              <Radio value="5">
+                Outro
+              </Radio>
+                <Textarea />
+            </Stack>
+          </RadioGroup>
+        </FormControl>
+      </Grid>
     </Flex>
   );
 };
