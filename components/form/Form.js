2f1f3d8de19c99804e9140485fe3217c7f615e1b
Igor Souza <soujigor@gmail.com>
2022-09-21 19:12:02 -0300

    arrumando o form hehe

--- components/form/Form.js
+++ components/form/Form.js
@@ -4,40 +4,46 @@ import {
   FormControl,
   FormLabel,
   Input,
-  NumberInput,
-  NumberInputField,
   Textarea,
   Stack,
   Radio,
   RadioGroup,
-  Grid,
+  SimpleGrid,
+  Button,
+  Box,
 } from "@chakra-ui/react";
 
 const formList = [
-  {
-    label: "Nome Completo",
-  },
-  {
-    label: "Whatsapp",
-  },
-  {
-    label: "Endereço Completo",
-  },
-  {
-    label: "Conte-me sobre sua rotina",
-  },
-  {
-    label: "Proteínas Preferidas",
-  },
-  {
-    label: "Legumes que não podem faltar",
-  },
-  {
-    label: "O que você não come de jeito nenhum",
-  },
-  {
-    label: "O prato que não pode faltar",
-  },
+  [
+    {
+      label: "Nome Completo",
+    },
+    {
+      label: "Whatsapp",
+    },
+    {
+      label: "Endereço Completo",
+    },
+  ],
+  [
+    {
+      label: "Conte-me sobre sua rotina",
+    },
+    {
+      label: "Proteínas Preferidas",
+    },
+    {
+      label: "Legumes que não podem faltar",
+    },
+  ],
+  [
+    {
+      label: "O que você não come de jeito nenhum",
+    },
+    {
+      label: "O prato que não pode faltar",
+    },
+  ],
 ];
 
 const radioList = [
@@ -114,25 +120,50 @@ const radioList = [
 const Form = () => {
   return (
     <Flex
-      backgroundColor="yellow"
+      backgroundColor="yellow.50"
       justifyContent="center"
       alignContent="center"
+      direction="column"
+      padding="20px"
     >
-      <Flex flexDirection="column" width="50%">
-        {formList.map((formItem) => {
+      {/* {formList.map((formItem) => {
           return (
             <FormControl isRequired key={formItem.label}>
               <FormLabel>{formItem.label}</FormLabel>
               <Textarea resize="none" />
             </FormControl>
           );
-        })}
-      </Flex>
+        })} */}
+      {formList.map((formItem, index) => {
+        return (
+          <Flex
+            key={index}
+            w="100%"
+            justify="space-between"
+            align="center"
+            gap="15px"
+            direction={["column", "column", "row", "row"]}
+          >
+            {formItem.map((item, index) => {
+              return (
+                <FormControl isRequired key={item.label}>
+                  <FormLabel>{item.label}</FormLabel>
+                  {item.label === "Whatsapp" ? (
+                    <Input type="number"></Input>
+                  ) : (
+                    <Textarea resize="none" type="number" />
+                  )}
+                </FormControl>
+              );
+            })}
+          </Flex>
+        );
+      })}
 
-      {/* <Flex>
+      <SimpleGrid columns={[1, 2, 3]} spacing="40px">
         {radioList.map((radioItem) => {
           return (
-            <FormControl isRequired key={radioItem.label}>
+            <FormControl isRequired key={radioItem.label} padding="10px">
               <FormLabel>{radioItem.label}</FormLabel>
               <RadioGroup defaultValue="">
                 <Stack
@@ -151,15 +182,15 @@ const Form = () => {
             </FormControl>
           );
         })}
-      </Flex> */}
-      {/* <Flex>
-        <FormControl isRequired>
+      </SimpleGrid>
+      <SimpleGrid padding="10px">
+        <FormControl isRequired padding="10px">
           <FormLabel>
             Qual é o melhor dia para agendarmos a ida na sua casa?
           </FormLabel>
           <Input placeholder="Select Date and Time" size="md" type="date" />
         </FormControl>
-        <FormControl isRequired>
+        <FormControl isRequired padding="10px">
           <FormLabel>
             Ah, para a minha organização preciso saber... Você tem pelo menos
             duas panelas grandes?
@@ -172,7 +203,12 @@ const Form = () => {
             </Stack>
           </RadioGroup>
         </FormControl>
-      </Flex> */}
+      </SimpleGrid>
+      <Flex justifyContent="center" alignItems="center">
+        <Button boxShadow="dark-lg" colorScheme="green" size="lg">
+          Enviar
+        </Button>
+      </Flex>
     </Flex>
   );
 };
